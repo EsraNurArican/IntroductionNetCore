112ca25ef80c3318ef277cfe9dfb5d68833edb33
esra <esrarican17@gmail.com>
2021-01-26 19:54:05 +0300

    Add project files.

---
+++ IntroNetCore/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Esra";
+            ViewBag.Time = DateTime.Now.Hour;
+
+            // sayfada göstermek için ürünler ekliyoruz
+            List<String> products = new List<string>();
+            products.Add("Pizza");
+            products.Add("Su");
+            products.Add("Dondurma");
+
+            ViewBag.Products = products;
+
+            return View();
+        }
+    }
+}
